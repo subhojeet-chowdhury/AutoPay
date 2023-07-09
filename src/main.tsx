839b90199ee09aeb536084a3c52023a9cf87eaeb
subhojeet chowdhury <subhojeetchowdhury98@gmail.com>
2023-07-09 16:18:39 +0530

    Configuring theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraBaseProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraBaseProvider>
+    <ChakraBaseProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraBaseProvider>
   </React.StrictMode>

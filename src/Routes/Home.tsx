dc3338283d9701da5a1dabff8eca398343eb98e5
subhojeet chowdhury <subhojeetchowdhury98@gmail.com>
2023-07-09 16:38:25 +0530

    Installing react router

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,13 @@
+import { Box, HStack } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <>
+      <Box backgroundColor="#5271ff">
+        <HStack></HStack>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraBaseProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraBaseProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <RouterProvider router={router} />
     </ChakraBaseProvider>
   </React.StrictMode>
 );
